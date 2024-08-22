ac287b592fccb7eb80230c298b9bb780895adb19
Gabrielle Tsai <linda41436@gmail.com>
2024-08-22 16:15:27 +0800

    精選文章 RWD.

--- main.js
+++ main.js
@@ -2,3 +2,33 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+const swiper = new Swiper('.article-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 48,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-custom',
+    type: 'fraction'
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+
+  breakpoints: {
+    375: {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerGroup: 2,
+      slidesPerView: 2
+    }
+  }
+});
